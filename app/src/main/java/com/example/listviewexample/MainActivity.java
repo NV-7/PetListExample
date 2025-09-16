c0fc77ce678019713281bcd6ffe227c4ff022370
Tommy <naviboun@gmail.com>
2025-09-16 12:01:03 -0500

    ListView adapter set up using string resource

--- app/src/main/java/com/example/listviewexample/MainActivity.java
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -3,13 +3,20 @@ package com.example.listviewexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        useStringResource();
 
         int selection = 1;
         populateListView(selection);
@@ -32,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
      * the content of a strings array resource
      */
     private void useStringResource() {
+        String[] petList = getResources().getStringArray(R.array.pet_list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, petList);
+        listView.setAdapter(adapter);
 
     }
 
